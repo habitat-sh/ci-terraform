65cc900b94f6e9b2b5d8bf5deb6636888c655f21
Jon Cowie <jonlives@gmail.com>
2018-04-03 17:05:40 +0100

    Add ingress rule for concourse workers

--- concourse.tf
+++ concourse.tf
@@ -84,6 +84,13 @@ resource "aws_security_group" "concourse_web_worker_sg" {
   }
 
   ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
